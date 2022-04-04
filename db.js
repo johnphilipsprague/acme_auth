f1f4fb9fd1168dd77f8d0fa034ca39cba432ef3e
johnphilipsprague <jpsprague95@gmail.com>
2022-04-04 15:36:56 -0500

    Resolved JWT bugs

--- db.js
+++ db.js
@@ -1,79 +1,90 @@
-const jsonWebToken = require('jsonwebtoken');
-
 // Contains the Sequelize data models and seeding code
-const Sequelize = require('sequelize');
-const { STRING } = Sequelize;
+
+const Sequelize = require("sequelize")
+const { STRING } = Sequelize
 const config = {
-  logging: false
-};
-
-const SECRET_KEY = process.env.JWT;
-
-if(process.env.LOGGING){
-  delete config.logging;
+	logging: false,
 }
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_db', config);
+const JWT = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+const saltRounds = 10
 
-const User = conn.define('user', {
-  username: STRING,
-  password: STRING
-});
+const SECRET_KEY = process.env.JWT
 
-User.byToken = async(token)=> {
-  try {
-    const user = await User.findByPk(token);
-    if(user){
-      return user;
-    }
-    const error = Error('bad credentials');
-    error.status = 401;
-    throw error;
-  }
-  catch(ex){
-    const error = Error('bad credentials');
-    error.status = 401;
-    throw error;
-  }
-};
+if (process.env.LOGGING) {
+	delete config.logging
+}
+const conn = new Sequelize(
+	process.env.DATABASE_URL || "postgres://localhost/acme_db",
+	config
+)
 
-User.authenticate = async({ username, password })=> {
-  const user = await User.findOne({
-    where: {
-      username,
-      password
-    }
-  });
-  if(user){
-    const token = JWT.sign({userId: user.id, SECRET_KEY})
-    return token;
-  }
-  const error = Error('bad credentials');
-  error.status = 401;
-  throw error;
-};
+const User = conn.define("user", {
+	username: STRING,
+	password: STRING,
+})
 
-const syncAndSeed = async()=> {
-  await conn.sync({ force: true });
-  const credentials = [
-    { username: 'lucy', password: 'lucy_pw'},
-    { username: 'moe', password: 'moe_pw'},
-    { username: 'larry', password: 'larry_pw'}
-  ];
-  const [lucy, moe, larry] = await Promise.all(
-    credentials.map( credential => User.create(credential))
-  );
-  return {
-    users: {
-      lucy,
-      moe,
-      larry
-    }
-  };
-};
+User.beforeCreate(async (user) => {
+	user.password = await bcrypt.hash(user.password, saltRounds)
+	console.log(user.password)
+})
+
+User.byToken = async (token) => {
+	try {
+		const validToken = JWT.verify(token, SECRET_KEY)
+		if (validToken) {
+			const user = await User.findByPk(validToken.user)
+			return user
+		}
+
+		const error = Error("bad credentials")
+		error.status = 401
+		throw error
+	} catch (ex) {
+		const error = Error("bad credentials")
+		error.status = 401
+		throw error
+	}
+}
+
+User.authenticate = async ({ username, password }) => {
+	const user = await User.findOne({
+		where: {
+			username,
+			password,
+		},
+	})
+	if (user) {
+		const token = JWT.sign({ userId: user.id }, SECRET_KEY)
+		return token
+	}
+	const error = Error("bad credentials")
+	error.status = 401
+	throw error
+}
+
+const syncAndSeed = async () => {
+	await conn.sync({ force: true })
+	const credentials = [
+		{ username: "lucy", password: "lucy_pw" },
+		{ username: "moe", password: "moe_pw" },
+		{ username: "larry", password: "larry_pw" },
+	]
+	const [lucy, moe, larry] = await Promise.all(
+		credentials.map((credential) => User.create(credential))
+	)
+	return {
+		users: {
+			lucy,
+			moe,
+			larry,
+		},
+	}
+}
 
 module.exports = {
-  syncAndSeed,
-  models: {
-    User
-  }
-};
+	syncAndSeed,
+	models: {
+		User,
+	},
+}
